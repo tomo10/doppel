fe0117879380f61e60f53b2da59efcb2c4df1ac0
tomo <thce28@gmail.com>
2024-04-02 18:44:00 +0100

    Add missing start link function in worker file

--- lib/doppel/worker.ex
+++ lib/doppel/worker.ex
@@ -1,6 +1,10 @@
 defmodule Doppel.Worker do
   use GenServer, restart: :transient
 
+  def start_link(_) do
+    GenServer.start_link(__MODULE__, :no_args)
+  end
+
   def init(:no_args) do
     Process.send_after(self(), :do_one_file, 0)
     {:ok, nil}
